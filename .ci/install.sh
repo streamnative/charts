ba82ec692bdbebccd5e0758ff33f745674f7402b
Sijie Guo <sijie@apache.org>
2020-03-21 00:15:18 -0700

    Test producer

--- .ci/install.sh
+++ .ci/install.sh
@@ -5,8 +5,32 @@ CHARTS_HOME=`cd ${BINDIR}/..;pwd`
 OUTPUT_BIN=${CHARTS_HOME}/output/bin
 HELM=${OUTPUT_BIN}/helm
 KUBECTL=${OUTPUT_BIN}/kubectl
+NAMESPACE=pulsar
+CLUSTER=pulsar-ci
 
+echo "Creating a kind cluster ..."
 ${CHARTS_HOME}/hack/kind-cluster-build.sh --name pulsar-ci -c 1 -v 10
+
+echo "Install the local storage provisioner"
 ${HELM} install local-storage-provisioner pulsar/charts/local-storage-provisioner
-${KUBECTL} create namespace pulsar
-${HELM} install --values ${BINDIR}/values-local-pv.yaml pulsar-ci pulsar/charts/pulsar
+
+WC=$(kubectl get pods --field-selector=status.phase=Running | grep local-storage-provisioner | wc -l)
+while [[ ${WC} -lt 1 ]]; do
+  echo ${WC};
+  sleep 5
+  kubectl get pods --field-selector=status.phase=Running
+done
+
+echo "Install the pulsar chart"
+${KUBECTL} create namespace ${NAMESPACE}
+${HELM} install --values ${BINDIR}/values-local-pv.yaml ${CLUSTER} pulsar/charts/pulsar
+
+WC=$(kubectl get pods --field-selector=status.phase=Running | grep ${CLUSTER}-proxy | wc -l)
+while [[ ${WC} -lt 1 ]]; do
+  echo ${WC};
+  sleep 5
+  kubectl get pods --field-selector=status.phase=Running
+done
+
+# test producer
+${KUBECTL} exec -n pulsar -it ${CLUSTER}-toolset-0 -- /pulsar/bin/pulsar-client produce test-topic
