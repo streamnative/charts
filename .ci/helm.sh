ef740d3259e880b56afc134f22f88bc55697012a
Yong Zhang <zhangyong1025.zy@gmail.com>
2021-08-13 10:37:27 +0800

    Test the ci

--- .ci/helm.sh
+++ .ci/helm.sh
@@ -179,6 +179,14 @@ function ci::upgrade_pulsar_chart() {
     echo "Upgrading the pulsar chart"
     ${HELM} repo add loki https://grafana.github.io/loki/charts
     ${HELM} dependency update ${CHARTS_HOME}/charts/pulsar
-    ${HELM} upgrade -n ${NAMESPACE} --values ${value_file} ${CLUSTER} ${CHARTS_HOME}/charts/pulsar
+    ${HELM} upgrade -n ${NAMESPACE} --values ${value_file} ${CLUSTER} ${CHARTS_HOME}/charts/pulsar --timeout 1h --debug
     ci::wait_pulsar_ready
+
+    WC=$(${KUBECTL} get pods -n ${NAMESPACE} --field-selector=status.phase=Running | grep ${CLUSTER}-bookie | wc -l)
+    while [[ ${WC} -lt 2 ]]; do
+      echo ${WC};
+      sleep 15
+      ${KUBECTL} get pods -n ${NAMESPACE}
+      WC=$(${KUBECTL} get pods -n ${NAMESPACE} --field-selector=status.phase=Running | grep ${CLUSTER}-bookie | wc -l)
+    done
 }
