1d640fa1def62561910d0ab6760ee6c56e3aa9a7
Guangning E <guangning@apache.org>
2022-03-15 19:53:13 +0800

    Expose console backend port (#526)
    Fixed #522

--- charts/sn-platform/conf/streamnative_console/init_instance.py
+++ charts/sn-platform/conf/streamnative_console/init_instance.py
@@ -3,9 +3,14 @@ import json
 import os
 host = os.getenv("STREAMNATIVE_CONSOLE_BACKEND_URL")
 loginPath = "/cloud-manager/login"
+username = os.getenv("VAULT_SUPER_USER_NAME")
+password = os.getenv("VAULT_SUPER_USER_PASSWORD")
+if not username or not password:
+    username = os.getenv("SUPER_USER_NAME")
+    password = os.getenv("SUPER_USER_PASSWORD")
 loginData = {
-    "username": os.getenv("VAULT_SUPER_USER_NAME"),
-    "password": os.getenv("VAULT_SUPER_USER_PASSWORD"),
+    "username": username,
+    "password": password,
     "type":"pulsar-vault-userpass"
 }
 headers = {
@@ -19,11 +24,17 @@ headers = {
 }
 
 instanceData = {"name": os.getenv("INSTANCE_NAME"), "broker": os.getenv("WEB_SERVICE_URL")}
+serviceData = {}
 if (os.getenv("KOP_SERVICE_URL")):
-    instanceData["service"] = json.dumps({"kopServiceUrl": os.getenv("KOP_SERVICE_URL")})
+    serviceData["kopServiceUrl"] = os.getenv("KOP_SERVICE_URL")
+if (os.getenv("FLINK_SERVICE_URL")):
+    serviceData["flinkServiceUrl"] = os.getenv("FLINK_SERVICE_URL")
+instanceData["service"] = json.dumps(serviceData)
+
 instancePath = "/cloud-manager/v1/instances"
 instanceResponse = requests.post(url = host + instancePath, data = json.dumps(instanceData), headers=headers)
-if (instanceResponse.ok):
+if (instanceResponse.ok and 'Add environment success' in instanceResponse.text):
     print("Add environment success")
 else:
-    print("Add environment failed" + instanceResponse.text)
+    print("Add environment failed " + instanceResponse.text)
+    exit(-1)
