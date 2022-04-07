41dfd0b4042e2a5c6aafb916133ef45bc1fa473a
guangning <guangning@apache.org>
2022-04-07 20:34:43 +0800

    Update version check

--- .ci/download_image.sh
+++ .ci/download_image.sh
@@ -27,18 +27,24 @@ components=(
     configmapReload
     external_dns)
 sn_platform_tag=$1
-curl https://raw.githubusercontent.com/streamnative/charts/${sn_platform_tag}/charts/sn-platform/values.yaml -o values.yaml
-image_list=""
-for i in ${components[@]}; do
-    repository=$(cat values.yaml | yq .images.$i.repository)
-    tag=$(cat values.yaml | yq .images.$i.tag)
-    echo "Downloading docker image: $repository:$tag"
-    image_list=image_list" $repository:$tag"
-    docker pull $repository:$tag
-done
+regexp="^sn-platform-([0-9]{1,3})\.([0-9]{0,3})\.([0-9]{0,3})$"
+if [[ $sn_platform_tag =~ $regexp ]]; then
+    curl https://raw.githubusercontent.com/streamnative/charts/${sn_platform_tag}/charts/sn-platform/values.yaml -o values.yaml
+    image_list=""
+    for i in ${components[@]}; do
+        repository=$(cat values.yaml | yq .images.$i.repository)
+        tag=$(cat values.yaml | yq .images.$i.tag)
+        echo "Downloading docker image: $repository:$tag"
+        image_list=image_list" $repository:$tag"
+        docker pull $repository:$tag
+    done
 
-mkdir -p $sn_platform_tag
-cd $sn_platform_tag
-# docker save -o $sn_platform_tag.tar $image_list
-# test
-docker save -o $sn_platform_tag.tar jimmidyson/configmap-reload:v0.3.0
+    mkdir -p $sn_platform_tag
+    cd $sn_platform_tag
+    # docker save -o $sn_platform_tag.tar $image_list
+    # test
+    docker save -o $sn_platform_tag.tar jimmidyson/configmap-reload:v0.3.0
+else
+    echo "Please use the correct version number format => sn-platform-x.x.x"
+    echo "Currently only sn-platform images are supported for upload"
+fi
