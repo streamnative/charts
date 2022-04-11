5d3c125628d0ecae2bf1eb4dbc222df545543ecf
guangning <guangning@apache.org>
2022-04-11 21:35:22 +0800

    Fixed image save command

--- .ci/download_image.sh
+++ .ci/download_image.sh
@@ -28,6 +28,8 @@ components=(
     external_dns)
 sn_platform_tag=$1
 regexp="^sn-platform-([0-9]{1,3})\.([0-9]{0,3})\.([0-9]{0,3})$"
+mkdir -p $sn_platform_tag
+cd $sn_platform_tag
 if [[ $sn_platform_tag =~ $regexp ]]; then
     curl https://raw.githubusercontent.com/streamnative/charts/${sn_platform_tag}/charts/sn-platform/values.yaml -o values.yaml
     image_list=""
@@ -39,11 +41,13 @@ if [[ $sn_platform_tag =~ $regexp ]]; then
         docker pull $repository:$tag
     done
 
-    mkdir -p $sn_platform_tag
-    cd $sn_platform_tag
-    # docker save -o $sn_platform_tag.tar $image_list
+    uniq_image_list=($(echo ${image_list[*]} | sed 's/ /\n/g'|sort| uniq))
+    for j in ${uniq_image_list[@]}; do
+        image=$(echo ${j} | sed 's/:/-/g');
+        docker save -o ${image}.tar ${j}
+    done
     # test
-    docker save -o $sn_platform_tag.tar jimmidyson/configmap-reload:v0.3.0
+    # docker save -o $sn_platform_tag.tar jimmidyson/configmap-reload:v0.3.0
 else
     echo "Please use the correct version number format => sn-platform-x.x.x"
     echo "Currently only sn-platform images are supported for upload"
